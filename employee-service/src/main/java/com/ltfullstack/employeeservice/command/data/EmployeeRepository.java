8dd73f238ccfe926fa868eae931b6ec06be4f9c4
tnguyenngocqa <tnguyenngocqa@gmail.com>
2026-01-04 20:04:02 +0700

    Get Employee - success

--- employee-service/src/main/java/com/ltfullstack/employeeservice/command/data/EmployeeRepository.java
+++ employee-service/src/main/java/com/ltfullstack/employeeservice/command/data/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.ltfullstack.employeeservice.command.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
+    List<Employee> findAllByIsDisciplined(Boolean isDisciplined);
 }
